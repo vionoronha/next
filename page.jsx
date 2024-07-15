4db2442871f83efcd5e68a5b70f3ad57548f2f1b
vionoronha <157267868+vionoronha@users.noreply.github.com>
2024-07-15 20:22:24 +0530

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,6 +1,7 @@
-"use client";
-import { usePathname } from "next/navigation";
-export default function Post() {
-  const pathname = usePathname(); //it access the path of dynamic route(url)
-  return <p> {pathname}</p>; //it has the current url's pathname
-}
+import React from "react";
+
+const page = () => {
+  return <div>page2305</div>;
+};
+
+export default page;
