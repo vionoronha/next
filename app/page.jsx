673fdeb57a8714d9c1d6fd7dd10385f02b04c9be
vionoronha <157267868+vionoronha@users.noreply.github.com>
2024-07-27 10:57:36 +0530

    Add files via upload
    Adding the folder and its contents

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return;
+  <>
+    <h1 className="text-blue-900">Viona Noronha</h1>
+    <p className="text-gray-700 ">
+      I am currently studying in St.Joseph's Engineering College.The course that
+      I have chosen is CSDS Engineering.
+    </p>
+    <p className="text-gray-700 ">The week 1 has been good so far.</p>
+  </>;
+};
+
+export default page;
